6dae7cbc0d53860ad55c78ea1d17638ba6ebc448
Bhooveshwaran Babu <bhooveshwaranb@gmail.com>
2026-05-10 12:52:02 +0900

    Add: max-water-container - submission-5

---
+++ Data Structures & Algorithms/max-water-container/submission-5.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        //HashMap Solution 1 - Brute Force
+        Map<Integer[],Integer> hashmap = new HashMap<>();
+
+        for(int i=0;i<heights.length;i++){
+            for(int j=0; j<heights.length;j++){
+                if(i==j)continue;
+                Integer[] index = new Integer[]{i,j};
+                Integer lengthMin = Math.min(heights[i],heights[j]);
+                Integer breadth = j-i;
+                Integer area = lengthMin * breadth;
+                hashmap.put(index,area);
+            }
+        }
+
+        return Collections.max(hashmap.values());
+        
+    }
+}
